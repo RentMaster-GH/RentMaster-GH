fda6fad606cca52b774eab72cdd69251a2ef73bf
RentMaster-GH <papastickle@googlemail.com>
2026-08-09 15:07:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,31 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+# GOOGLE SITE VERIFICATION + GTM INJECTION
+components.html(
+    """
+    <!-- Google Site Verification - HTML TAG METHOD -->
+    <meta name="google-site-verification" content="25ade72139f11424" />
+
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M8WLPHJW');
+    </script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8WLPHJW"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    """,
+    height=0,
+    width=0,
+)
+
 # 3.5 INJECT GTM + GA4 + SITE VERIFICATION (<head> & <body> NOSCRIPT)
 components.html(
     """
